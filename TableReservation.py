640f1307bf0980a87bf3cc75e9148eddcfa016a8
Sirisak Lueangsaksri <sirisak.lu@tnis.com>
2016-04-20 18:04:40 +0700

    Supports custom random seed, simulation speed and more stats

--- TableReservation.py
+++ TableReservation.py
@@ -8,6 +8,10 @@ except:
     pass
 
 
+################################################################################
+
+simulation_speed = 0.1
+
 food_shops = [
     {
         "name": "Drink",
@@ -71,7 +75,16 @@ flow_speed = [1, 2, 2, 3, 3, 4, 5]
 #   4 persons possibility: 1/7
 group_sizes = [1, 1, 1, 2, 2, 3, 4, 5, 6]
 
+# TODO
+# - wait_for_someone_in_shop = True
+#   - wait_even_table_is_free = True
+#   - maximum_waiting_time = -1
+# - start_eating_at_the_same_time = False
+
 ################################################################################
+
+seed = input("Enter seed number: ")
+random.seed(seed if seed else None)
 
 name_list = [
     "Somkiat", "Sompong", "Somnamna", "Somsak", "Somsri", "Somreudee",
@@ -83,6 +96,7 @@ wait_queues = []
 last_group_size = 0
 next_group = None
 maximum = {
+    "waiting_queue": 0,
     "waiting_time": 0,
     "occupied_tables": 0
 }
@@ -94,6 +108,7 @@ while True:
     # Clear screen
     print("\n" * 80)
 
+    print("Maximum waiting queue: %s" % (maximum["waiting_queue"]))
     print("Maximum waiting time: %.2fs" % (maximum["waiting_time"]))
     print("Maximum occupied tables: %s" % (maximum["occupied_tables"]))
     next_group_time_diff = next_group - time.time() if next_group else 0
@@ -103,10 +118,13 @@ while True:
     ))
     shop_row = ""
     for index, shop in enumerate(food_shops):
+        serving_time_diff = (
+            shop["serving_end_time"] - time.time()
+            if "serving_end_time" in shop else 0
+        )
         shop_row += "%10s [%2.2fs : %2d]%s" % (
             shop["name"],
-            shop["serving_end_time"] - time.time()
-            if "serving_end_time" in shop else 0,
+            serving_time_diff if serving_time_diff > 0 else 0,
             len(shop["queues"]) if "queues" in shop else 0,
             " " if index != 0 else ""
         )
@@ -170,7 +188,7 @@ while True:
                 "uuid": str(uuid.uuid4()),
                 "group_id": group_id
             })
-        next_group = time.time() + random.choice(flow_speed)
+        next_group = time.time() + random.choice(flow_speed) * simulation_speed
     # Update shop queues
     for shop in food_shops:
         # If order is served, serve the next person
@@ -178,14 +196,18 @@ while True:
             "serving_end_time" not in shop and
             "queues" in shop and len(shop["queues"]) > 0
         ):
-            shop["serving_end_time"] = time.time() + shop["serving_time"]
+            shop["serving_end_time"] = time.time() + (
+                shop["serving_time"] * simulation_speed
+            )
         elif (
             "serving_end_time" in shop and
             shop["serving_end_time"] - time.time() <= 0
         ):
             occupation = shop["queues"].pop()
             occupation["queue_time"] = time.time()
-            occupation["eating_duration"] = random.choice(shop["eating_time"])
+            occupation["eating_duration"] = (
+                random.choice(shop["eating_time"]) * simulation_speed
+            )
             wait_queues.append(occupation)
             del shop["serving_end_time"]
 
@@ -253,6 +275,8 @@ while True:
 
     # Calculate maximum
     if wait_queues:
+        if maximum["waiting_queue"] < len(wait_queues):
+            maximum["waiting_queue"] = len(wait_queues)
         wait_time = time.time() - (
             wait_queues[0]["queue_time"]
             if "queue_time" in wait_queues[0] else time.time()
